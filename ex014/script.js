a294c8e37ca1642769aa401e64c834d1a40e75e4
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-04-09 15:54:15 -0300

    ex14 b

--- ex014/script.js
+++ ex014/script.js
@@ -1,9 +1,19 @@
-var agora = new Date()
-var diasem = agora.getHours()
-var res = document.getElementById('res')
-
-
-if (diasem <= 12) {
-    res.innerHTML = `Agora são ${diasem} horas.`
-    body.style.background = '#aac72b9f'
+function carregar() {
+    var msg = document.getElementById('msg')
+    var imagem = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        imagem.src = 'bom-dia.jpeg'
+        document.body.style.background = 'rgba(255, 255, 0, 0.432)'
+    }else if (hora >=12 && hora <= 18){
+        imagem.src = 'boa-tarde.jpg'
+        document.body.style.background = 'orange'
+    }else {
+        imagem.src = 'boa-noite.jpg'
+        document.body.style.background = 'gray'
+    }
 }
+
