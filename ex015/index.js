00e9ba6f2018ea60d13dc20c3603f5657d124686
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-04-14 17:33:15 -0300

    ex15

---
+++ ex015/index.js
@@ -0,0 +1,15 @@
+// teste logico no inicio
+
+
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+// teste logico no final
+var d = 1
+do {
+    console.log(`Número ${d}`)
+    d++
+} while(d <= 10)
